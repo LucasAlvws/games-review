11f38686d9dafac5c5be1d75d8000d52e19d2560
Lucas Alves <sociallucasalves@gmail.com>
2025-02-05 12:54:26 -0300

    feat: arquivos de deploy no heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn games_review.wsgi -- log-file - 
--- games_review/settings.py
+++ games_review/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY", cast=str, default='')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool, default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
